9e73953ac4c8aac30c9fa10117a6ab24bd76fe11
Heitor <heitorvmacedo@gmail.com>
2023-11-30 18:03:47 -0300

    initializing bird class

--- flappyBird.py
+++ flappyBird.py
@@ -16,3 +16,46 @@ IMGS_BIRD = [
 
 pygame.font.init()
 SCORE_FONT = pygame.font.SysFont('Flappy Bird Font', 50)
+
+class Bird:
+    IMGS = IMGS_BIRD
+
+    # Rotations animation
+    MAX_ROTATION = 25
+    ROTATION_SPEED = 20
+    ANIMATION_TIME = 5
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        self.angle = 0
+        self.speed = 0
+        self.height = self.y
+
+        self.time = 0
+        self.img_count = 0
+        self.img = IMGS[0]
+
+    def jump(self):
+        self.speed = -10.5
+        self.time = 0
+        self.height = self.y
+
+    def move(self):
+
+        # Calculating distance
+
+        # Restricting distance
+
+        
+        # Bird angle
+
+
+
+
+class Pipe:
+    pass
+
+class Base:
+    pass
